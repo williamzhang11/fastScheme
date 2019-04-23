c25ee9aca69ace7c67b756711c793dafe65b2279
williamzhang11 <1107517306@qq.com>
2019-04-23 23:01:43 +0800

    增加springcloud微服务分布式架构设计方案

---
+++ springcloud微服务框架/客户端负载均衡器与熔断器/ribbon-server1/src/main/java/com/xiu/register/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.xiu.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
